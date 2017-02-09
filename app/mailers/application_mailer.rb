91377d377419191b97c961827edf7e1f4d8233b7
Ankur Sethi <sethi_ankur@bah.com>
2017-02-09 15:25:27 -0500

    Add test email Fix email setup
    Change default from setting so it will pick up changes to AppSettings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,21 +2,20 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   before_action :setup_mailer
+  # Set the default sender; Change as needed in each mailer method
+  default from: Proc.new { AppSetting.current.mail_sender }
 
   def setup_mailer
     settings = AppSetting.current
 
-    # Set the default sender; Change as needed in each mailer method
-    mail.from = settings.mail_sender
-
     # Should be little need to override any of these settings in the mailer methods
-    mail.delivery_method.settings.merge!({
+    ActionMailer::Base.smtp_settings = {
       address:             settings.mail_host,
       port:                settings.mail_port,
       user_name:           settings.mail_username,
       password:            settings.mail_password,
       authentication:      settings.mail_authentication,
       openssl_verify_mode: settings.mail_ssl_verify
-    })
+    }
   end
 end
